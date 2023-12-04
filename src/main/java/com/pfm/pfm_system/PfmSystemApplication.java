f7c03ad2608b10b10909f2c3cebdd67b33d45893
EduardoSegarraLedesma <eduardo.segarra.ledesma@alumnos.upm.es>
2023-12-04 22:40:09 +0800

    main class update

--- src/main/java/com/pfm/pfm_system/PfmSystemApplication.java
+++ src/main/java/com/pfm/pfm_system/PfmSystemApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PfmSystemApplication {
     public static void main(String[] args) {
-        // http://localhost:8080/index.html
-        ServerController.getInstance();
         SpringApplication.run(PfmSystemApplication.class, args);
     }
 }
