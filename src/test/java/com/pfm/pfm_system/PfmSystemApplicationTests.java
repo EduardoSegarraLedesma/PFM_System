51a59b24616b8929d9ed28a5488dec1251b215a8
EduardoSegarraLedesma <eduardo.segarra.ledesma@alumnos.upm.es>
2023-12-04 23:05:59 +0800

    Application tests update for Azure CI

--- src/test/java/com/pfm/pfm_system/PfmSystemApplicationTests.java
+++ src/test/java/com/pfm/pfm_system/PfmSystemApplicationTests.java
@@ -1,13 +1,20 @@
 package com.pfm.pfm_system;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class PfmSystemApplicationTests {
 
+    @Autowired
+    private PfmSystemApplication application;
+
     @Test
     void contextLoads() {
+        assertNotNull(application);
     }
 
 }
